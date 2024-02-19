3d93cff1663cba698c0535ad4738ad1c5be14778
bantheus <matheuschmidt.b@gmail.com>
2024-02-19 00:30:20 -0300

    ✨ Feat: add course table component and course interface

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
-})
-export class CoursesComponent {
+  styleUrl: './courses.component.scss',
 
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    { _id: "1", name: "Angular", category: "Frontend" },
+  ];
+  displayedColumns = ["name", "category"]
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
